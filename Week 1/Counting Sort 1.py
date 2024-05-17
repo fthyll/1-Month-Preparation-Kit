02108bbdb8b1c482e644348b1015f8c147eda18f
Muhammad Fatih Yumna Lajuwirdi Lirrahman <m.fatihyumna25@gmail.com>
2024-05-17 18:17:51 +0700

    Counting Sort 1

---
+++ Week 1/Counting Sort 1.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'countingSort' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def countingSort(arr):
+    # Create a frequency array of size 100 (0 to 99)
+    frequency = [0] * 100
+    
+    # Populate the frequency array
+    for num in arr:
+        frequency[num] += 1
+    
+    return frequency
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = countingSort(arr)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
